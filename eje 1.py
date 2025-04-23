2158fac053d975b87256e9446b752fa96896e748
zoeflaite <zoemalencita@gmail.com>
2025-04-23 08:56:24 -0300

    Add files via upload

---
+++ eje 1.py
@@ -0,0 +1,12 @@
+import tkinter  as tk
+def main():
+    #crear ventana principal
+    ventana = tk.Tk ()
+    ventana.title("Feli y Zoe")
+    ventana.geometry("1000x655")
+    ventana.configure(bg="violet")
+    ventana.mainloop()
+    
+
+if __name__=="__main__" :
+    main() 
